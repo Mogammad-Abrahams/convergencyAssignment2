c38732777f1571c212b0978e7f75771e5a2829d3
Mogammad-Abrahams <56668467+Mogammad-Abrahams@users.noreply.github.com>
2020-03-17 11:14:57 +0200

    Add files via upload

---
+++ Convergency/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+
+namespace Convergency
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long[] fib = { 2, 0 };
+            int i = 0;
+            long summed = 0;
+
+            while (fib[i] < 5000000)
+            {
+                summed += fib[i];
+                i = (i + 1) % 2;
+                fib[i] = 4 * fib[(i + 1) % 2] + fib[i];
+            }
+            Console.WriteLine(summed);
+        }
+    }
+}
